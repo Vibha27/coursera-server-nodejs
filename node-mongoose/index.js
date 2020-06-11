cf970f8f9eb0e34c58b1ea2b4575d05c8f805c09
vibha singh <svibha081@gmail.com>
2020-06-11 15:46:26 +0530

    mongoose part2

---
+++ node-mongoose/index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected to mongo server");
+
+    Dishes.create({
+        name : 'Uthappizza',
+        description : 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        return Dishes.find({}).exec()
+    })
+    .then((dishes) => {
+        console.log(dishes);
+        return Dishes.remove({})
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => console.log(err))
+})
